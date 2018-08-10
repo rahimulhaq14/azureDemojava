fbe788a97990039708a49a07e1d5c51c4d002a58
umar-muneer <umar.muneer@educative.io>
2018-08-10 16:37:00 +0500

    updated code

--- src/main/java/azureForDemo/azureDemo/demo.java
+++ src/main/java/azureForDemo/azureDemo/demo.java
@@ -25,6 +25,7 @@ import com.microsoft.azure.management.resources.fluentcore.model.Indexable;
 import com.microsoft.azure.management.resources.fluentcore.utils.SdkContext;
 import com.microsoft.rest.LogLevel;
 import rx.Observable;
+import rx.Subscription;
 import rx.schedulers.Schedulers;
 
 import java.io.File;
@@ -47,11 +48,15 @@ public final class demo {
             final String IP_NAME = RG_NAME + "-ip1";
             final String SUBNET_NAME = RG_NAME + "-subnet1";
             final String VIRTUAL_NETWORK_NAME = RG_NAME + "-vn1";            
+            final String NIC_NAME = RG_NAME + "-nic1";
+            final String VM_NAME = RG_NAME + "-vm1";
             Azure azure = getAzureClient();
             createResourceGroup(azure, RG_NAME);
-            createAvailabilitySet(azure, RG_NAME, AV_NAME);
-            createPublicIPAddress(azure, RG_NAME, IP_NAME);
-            createVirtualNetwork(azure, RG_NAME, SUBNET_NAME, VIRTUAL_NETWORK_NAME);
+            AvailabilitySet avSet = createAvailabilitySet(azure, RG_NAME, AV_NAME);
+            PublicIPAddress ip = createPublicIPAddress(azure, RG_NAME, IP_NAME);
+            Network network = createVirtualNetwork(azure, RG_NAME, SUBNET_NAME, VIRTUAL_NETWORK_NAME);
+            NetworkInterface nic = createNIC(azure, RG_NAME, ip.id(), network.id(), SUBNET_NAME, NIC_NAME);
+            createVM(azure, RG_NAME, nic.id(), avSet.id(), VM_NAME);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
@@ -76,7 +81,7 @@ public final class demo {
             .create();
         System.out.println("created resource group" + resourceGroup.name());
     } 
-    public static void createAvailabilitySet(Azure azure, String resourceGroup, String name) {
+    public static AvailabilitySet createAvailabilitySet(Azure azure, String resourceGroup, String name) {
         System.out.println("Creating availability set...");
         AvailabilitySet availabilitySet = azure.availabilitySets()
             .define(name)
@@ -84,9 +89,10 @@ public final class demo {
             .withExistingResourceGroup(resourceGroup)
             .withSku(AvailabilitySetSkuTypes.MANAGED)
             .create();
-        System.out.println("created availability set " + availabilitySet.name());
+        System.out.println("created availability set " + availabilitySet.id());
+        return availabilitySet;
     }
-    public static void createPublicIPAddress(Azure azure, String resourceGroup, String name) {
+    public static PublicIPAddress createPublicIPAddress(Azure azure, String resourceGroup, String name) {
         System.out.println("Creating public IP address...");
         PublicIPAddress publicIPAddress = azure.publicIPAddresses()
             .define(name)
@@ -94,9 +100,10 @@ public final class demo {
             .withExistingResourceGroup(resourceGroup)
             .withDynamicIP()
             .create();
-        System.out.println("created ip address " + publicIPAddress.name());
+        System.out.println("created ip address with id:" + publicIPAddress.id());
+        return publicIPAddress;
     }
-    public static void createVirtualNetwork(Azure azure, String resourceGroup, String subnetName, String name) {
+    public static Network createVirtualNetwork(Azure azure, String resourceGroup, String subnetName, String name) {
         System.out.println("Creating virtual network...");
         Network network = azure.networks()
             .define(name)
@@ -105,57 +112,53 @@ public final class demo {
             .withAddressSpace("10.0.0.0/16")
             .withSubnet(subnetName,"10.0.0.0/24")
             .create();
-        System.out.println("created virtual network" + network.name());
+        System.out.println("created virtual network with id" + network.id());
+        return network;
     }
-    public static void createVM() {
+    public static NetworkInterface createNIC(Azure azure, String resourceGroup, String ipId, String networkId, String subnetName, String name) {
+        System.out.println("Creating network interface...");
+
+        PublicIPAddress ip = azure.publicIPAddresses().getById(ipId);
+        Network network = azure.networks().getById(networkId);
+        NetworkInterface networkInterface = azure.networkInterfaces()
+            .define(name)
+            .withRegion(Region.US_EAST)
+            .withExistingResourceGroup(resourceGroup)
+            .withExistingPrimaryNetwork(network)
+            .withSubnet(subnetName)
+            .withPrimaryPrivateIPAddressDynamic()
+            .withExistingPrimaryPublicIPAddress(ip)
+            .create();
+        System.out.println("created network interface with id: " + networkInterface.id());
+        return networkInterface;
+    }
+
+    public static void createVM(Azure azure, String resourceGroup, String nicId, String avSetId, String name) {
         try {
             System.out.println("Start Creating a VM");
-        
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            // System.out.println("Creating network interface...");
-            // NetworkInterface networkInterface = azure.networkInterfaces()
-            //     .define("myNIC1")
-            //     .withRegion(Region.US_EAST)
-            //     .withExistingResourceGroup("myResourceGroup1")
-            //     .withExistingPrimaryNetwork(network)
-            //     .withSubnet("mySubnet")
-            //     .withPrimaryPrivateIPAddressDynamic()
-            //     .withExistingPrimaryPublicIPAddress(publicIPAddress)
-            //     .create();
-            // System.out.println("Creating VM....");
-            
-            
-            // Observable<Indexable> vmObservable = azure.virtualMachines()
-            //         .define("myVM1")
-            //         .withRegion(Region.US_EAST)
-            //         .withExistingResourceGroup("myResourceGroup1")
-            //         .withExistingPrimaryNetworkInterface(networkInterface)
-            //         .withLatestWindowsImage("MicrosoftWindowsServer", "WindowsServer", "2012-R2-Datacenter")
-            //         .withAdminUsername("azureuser")
-            //         .withAdminPassword("Azure12345678")
-            //         .withComputerName("myVM")
-            //         .withExistingAvailabilitySet(availabilitySet)
-            //         .withSize(VirtualMachineSizeTypes.STANDARD_D3_V2)
-            //         .createAsync();
-            
-            // vmObservable.subscribe();
-            /*Can uncomment when needed*/
-            
-
-            // System.out.println("created vm async");
-            // Thread.sleep(1000);
+            NetworkInterface nic = azure.networkInterfaces().getById(nicId);
+            AvailabilitySet avSet = azure.availabilitySets().getById(avSetId);
+            Observable<Indexable> vmObservable = azure.virtualMachines()
+                    .define(name)
+                    .withRegion(Region.US_EAST)
+                    .withExistingResourceGroup(resourceGroup)
+                    .withExistingPrimaryNetworkInterface(nic)
+                    .withLatestWindowsImage("MicrosoftWindowsServer", "WindowsServer", "2012-R2-Datacenter")
+                    .withAdminUsername("azureuser")
+                    .withAdminPassword("Azure12345678")
+                    .withComputerName("myVM")
+                    .withExistingAvailabilitySet(avSet)
+                    .withSize(VirtualMachineSizeTypes.BASIC_A0)
+                    .createAsync();
+            System.out.println("created vm ");
+            Thread.sleep(2000);
         }
         catch(Exception e) {
-            System.out.println("exception caught");
+            System.out.println(e.toString());
         }
+    }
+
+    public static void DeleteResourceGroup(Azure azure, String resourceGroup) {
+        azure.resourceGroups().deleteByName(resourceGroup);
     }
 }
